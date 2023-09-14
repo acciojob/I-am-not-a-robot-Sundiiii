71ade3c75d9c451e4d4af1207e1ca39633a96c9d
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-09-14 20:15:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const test = document.getElementsByTagName("h2");
 // const test = document.getElementsByTagName("a")(1);
 test.addEventListener(
-  "mouseover",  (event) => {
-    event.target.style.color = red;
-  alert("hiiiiiiiiiiiiiii");});
+  "mouseover",  () => {
+    test.style.background = "red";
+  // alert("hiiiiiiiiiiiiiii");});
  // document.write("gsdgjsefeadfchskdcsd") 
