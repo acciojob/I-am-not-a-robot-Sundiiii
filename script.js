1eec69951574da6cdf51cc230a1a2aa8c3cd80aa
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-09-15 14:07:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const test3 = document.getElementsByClassName("img4")[0];
 const test4 = document.getElementsByClassName("img5")[0];
 // const test = document.getElementsByTagName("a")(1);
 test.addEventListener("click", changeborder);
+test1.addEventListener("click", changeborder);
+test2.addEventListener("click", changeborder);
+test3.addEventListener("click", changeborder);
+test4.addEventListener("click", changeborder);
  function changeborder(x){
-x.style.border = "5px solid red";  
+x.target.style.border = "5px solid red";  
   }
-// changeborder();
